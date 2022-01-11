47503fdabee004835b385d267a047c1a5bfdb242
Natchii59 <caron.nathan@hotmail.com>
2022-01-11 15:30:55 +0100

    Add NotFound

--- src/pages/NotFound/NotFound.jsx
+++ src/pages/NotFound/NotFound.jsx
@@ -1,9 +1,17 @@
 import "./NotFound.css";
+import { Link } from "react-router-dom";
 
 const NotFound = () => {
   return (
-    <div>
-      <h1>Error 404 - Not found</h1>
+    <div className="notfound">
+      <div className="nf-text">
+        <span className="nf-title">404</span>
+        <span className="nf-desc">Il me semble que vous vous êtes égaré</span>
+      </div>
+
+      <Link to="/" className="nf-button">
+        Retour en lieu sûr
+      </Link>
     </div>
   );
 };
